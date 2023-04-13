247028195c6b2d0967d680214c18fe4b5007f866
Tom Heaton <thomas.m.heaton@gmail.com>
2023-04-13 15:16:08 +0100

    deezer footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <footer className="container mx-auto p-4">
+    <footer className="container mx-auto flex items-center justify-center space-x-2 p-4">
       <p className="text-center">
         made with ❤️ by{" "}
         <a
@@ -10,8 +12,25 @@ export default function Footer() {
           className="transition-colors hover:text-violet-500"
         >
           Tom Heaton
-        </a>
+        </a>{" "}
+        using
       </p>
+      <a href="https://deezer.com" target="_blank" rel="external noreferrer">
+        <Image
+          src="/Colored_Full_Black@2x.png"
+          alt="Deezer Logo"
+          width={895}
+          height={175}
+          className="block h-4 w-auto dark:hidden"
+        />
+        <Image
+          src="/Colored_Full_White@2x.png"
+          alt="Deezer Logo"
+          width={895}
+          height={175}
+          className="hidden h-4 w-auto dark:block"
+        />
+      </a>
     </footer>
   );
 }
